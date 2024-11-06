17d60d5a5567abee0a4ae039899502d8e1b7ee15
PranavPandey09 <pranavpandey2309@gmail.com>
2024-11-06 14:34:04 +0530

    update

--- routes/emailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { sendConfirmationEmail } = require("../controllers/emailController");
-const router = express.Router();
-
-router.post("/api/email", sendConfirmationEmail);
-
-module.exports = router;
